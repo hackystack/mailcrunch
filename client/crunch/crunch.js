92fd1f65210e7ace485de0e0ffeaff1d9ec407d8
hackystack <dorpalenbarry@gmail.com>
2014-07-08 11:39:13 -0700

    Working on dirt checking

--- client/crunch/crunch.js
+++ client/crunch/crunch.js
@@ -17,19 +17,37 @@ angular.module('myApp.main.crunch', ['ui.router'])
 	  to: 'Liam',
 	  subject: 'Im a grouch',
 	  body: 'Its so cloudy outside',
-	  time: 1215
+	  time: '12:15'
 	  },
 	  {from: 'Elmo',
 	  to: 'Liam',
 	  subject: 'Youve been served',
 	  body: 'Please come to court on 8/21 @ 9am',
-	  time: 0135
+	  time: '01:35'
 	  },
 	  {from: 'CookieMonster',
 	  to: 'Liam',
 	  subject: 'Cooookkkkiieeee!',
 	  body: 'Me want cooookkkkiieeee now',
-	  time: 1022
+	  time: '10:22'
+	  },
+	  {from: 'Oscar',
+	  to: 'Liam',
+	  subject: 'Hey',
+	  body: 'Whats your nick name',
+	  time: '13:15'
+	  },
+	  {from: 'Elmo',
+	  to: 'Liam',
+	  subject: 'Whattt',
+	  body: 'I dont understand why youd do that',
+	  time: '01:37'
+	  },
+	  {from: 'Josh',
+	  to: 'Liam',
+	  subject: 'Just checking in',
+	  body: 'Have you been to sushirrito',
+	  time: '10:25'
 	  }
 	]
     var emailIndex = 0;
@@ -38,11 +56,10 @@ angular.module('myApp.main.crunch', ['ui.router'])
 
 // this controller decrements the timeLeft variable once per second
 // TODO: add in a function that switches emails when timeLeft = 0;
-.controller('timeLeft',function($scope){
-  $scope.timeLeft = 5;
-  setInterval(1000, function(){
-  	if($scope.timeLeft >= 0){
-  	  $scope.timeLeft--
+.controller('crunchTimeLeft',function($scope,$interval){
+  $interval(function(){
+  	if($scope.timeLeft > 0){
+  	  $scope.timeLeft--;
     }
-  });
+  },120000);
 })
--- client/note/note.js
+++ client/note/note.js
@@ -13,24 +13,42 @@ angular.module('myApp.main.note', ['ui.router'])
 //this is dummy data to test the list of inbox emails	
 .controller('NoteController', function($scope) {
 	$scope.inbox =[
-	  {from: 'Oscar',
-	  to: 'Liam',
-	  subject: 'Im a grouch',
-	  body: 'Its so cloudy outside',
-	  time: 1215
-	  },
-	  {from: 'Elmo',
-	  to: 'Liam',
-	  subject: 'Youve been served',
-	  body: 'Please come to court on 8/21 @ 9am',
-	  time: 0135
-	  },
-	  {from: 'CookieMonster',
-	  to: 'Liam',
-	  subject: 'Cooookkkkiieeee!',
-	  body: 'Me want cooookkkkiieeee now',
-	  time: 1022
-	  }
+    {from: 'Oscar',
+    to: 'Liam',
+    subject: 'Im a grouch',
+    body: 'Its so cloudy outside',
+    time: '12:15'
+    },
+    {from: 'Elmo',
+    to: 'Liam',
+    subject: 'Youve been served',
+    body: 'Please come to court on 8/21 @ 9am',
+    time: '01:35'
+    },
+    {from: 'CookieMonster',
+    to: 'Liam',
+    subject: 'Cooookkkkiieeee!',
+    body: 'Me want cooookkkkiieeee now',
+    time: '10:22'
+    },
+    {from: 'Oscar',
+    to: 'Liam',
+    subject: 'Hey',
+    body: 'Whats your nick name',
+    time: '13:15'
+    },
+    {from: 'Elmo',
+    to: 'Liam',
+    subject: 'Whattt',
+    body: 'I dont understand why youd do that',
+    time: '01:37'
+    },
+    {from: 'Josh',
+    to: 'Liam',
+    subject: 'Just checking in',
+    body: 'Have you been to sushirrito',
+    time: '10:25'
+    }
 	]
     
     $scope.emailIndex = 0;
@@ -38,26 +56,34 @@ angular.module('myApp.main.note', ['ui.router'])
     
     $scope.sortManage = function(){
       $scope.inbox[$scope.emailIndex]['bucket'] = 'manage';
-      console.log($scope.inbox[$scope.emailIndex]);
+      console.log($scope.currentEmail);
       $scope.emailIndex++;
+      $scope.currentEmail = $scope.inbox[$scope.emailIndex]
+      console.log($scope.currentEmail);
       $scope.timeLeft = 5;
     };
     $scope.sortFocus = function(){
       $scope.inbox[$scope.emailIndex]['bucket'] = 'focus';
-      console.log($scope.inbox[$scope.emailIndex]);
+      console.log($scope.currentEmail);
       $scope.emailIndex++;
+      $scope.currentEmail = $scope.inbox[$scope.emailIndex]
+      console.log($scope.currentEmail);
       $scope.timeLeft = 5;
     };
     $scope.sortAvoid = function(){
       $scope.inbox[$scope.emailIndex].bucket = 'avoid';
-      console.log($scope.inbox[$scope.emailIndex]);
+      console.log($scope.currentEmail);
       $scope.emailIndex++;
+      $scope.currentEmail = $scope.inbox[$scope.emailIndex]
+      console.log($scope.currentEmail);
       $scope.timeLeft = 5;
     };
     $scope.sortLimit = function(){
       $scope.inbox[$scope.emailIndex]['bucket'] = 'avoid';
-      console.log($scope.inbox[$scope.emailIndex]);
+      console.log($scope.currentEmail);
       $scope.emailIndex++;
+      $scope.currentEmail = $scope.inbox[$scope.emailIndex]
+      console.log($scope.currentEmail);
       $scope.timeLeft = 5;
     };
 
