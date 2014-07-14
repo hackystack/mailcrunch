1940a92caaaf06a6347a2a96e07d02af6c5646ef
hackystack <dorpalenbarry@gmail.com>
2014-07-14 10:09:09 -0700

    Removed line about mongoose for deployment

--- server/main/config.js
+++ server/main/config.js
@@ -5,7 +5,7 @@ var mongoose    = require('mongoose'),
     bodyParser  = require('body-parser'),
     middle      = require('./middleware');
 
-mongoose.connect(process.env.DB_URL || 'mongodb://localhost/myApp');
+// mongoose.connect(process.env.DB_URL || 'mongodb://localhost/myApp');
 /*
  * Include all your global env variables here.
 */
